7d229da0b0c4ab989b8f0446adfd95c56ce24478
web-studio <vanechka_petrov_1996@list.ru>
2014-02-19 10:53:52 +0400

    промо

--- public_html/themes/profit/views/private/default/promotional_materials.php
+++ public_html/themes/profit/views/private/default/promotional_materials.php
@@ -1,22 +1,32 @@
+<?php
+$this->pageTitle = 'Promo materials';
+
+$this->breadcrumbs=array(
+    'My account'=>array('/private'),
+    'Referral program'=>array('/private/default/referrals'),
+    $this->pageTitle
+);
+?>
+
 <div class="promo_materials">
 
-    <img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/promo/125x125.gif' ?>" /><br />
+    <img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/images/promo/125x125.png' ?>" /><br />
     <textarea cols="30" rows="10"><a href="<?php echo Yii::app()->getRequest()->getHostInfo().'/?partner='.$user->id ?>" title="<?php echo Yii::app()->name; ?>">
-<img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/promo/125x125.gif' ?>" />
+<img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/images/promo/125x125.png' ?>" />
 </a></textarea>
 
     <span class="hr"></span>
 
-    <img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/promo/240x400.gif' ?>" /><br />
+    <img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/images/promo/240x400.png' ?>" /><br />
     <textarea cols="30" rows="10"><a href="<?php echo Yii::app()->getRequest()->getHostInfo().'/?partner='.$user->id ?>" title="<?php echo Yii::app()->name; ?>">
-<img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/promo/240x400.gif' ?>" />
+<img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/images/promo/240x400.png' ?>" />
 </a></textarea>
 
     <span class="hr"></span>
 
-    <img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/promo/468x70.gif' ?>" /><br />
+    <img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/images/promo/468x70.png' ?>" /><br />
     <textarea cols="30" rows="10"><a href="<?php echo Yii::app()->getRequest()->getHostInfo().'/?partner='.$user->id ?>" title="<?php echo Yii::app()->name; ?>">
-<img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/promo/468x60.gif' ?>" />
+<img src="<?php echo Yii::app()->getRequest()->getHostInfo().'/images/promo/468x70.png' ?>" />
 </a></textarea>
     <span class="hr"></span>
 </div>
