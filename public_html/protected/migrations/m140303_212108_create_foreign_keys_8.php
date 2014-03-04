b134506017c448ca63813e4fb476a9d047f35b42
web-studio <vanechka_petrov_1996@list.ru>
2014-03-04 11:12:07 +0400

    banners

--- public_html/protected/migrations/m140303_212108_create_foreign_keys_8.php
+++ public_html/protected/migrations/m140303_212108_create_foreign_keys_8.php
@@ -5,7 +5,7 @@ class m140303_212108_create_foreign_keys_8 extends CDbMigration
 	public function up()
 	{
 
-
+       // $this->addForeignKey('user_deposit_type_id','{{user_deposits}}', 'deposit_type_id', '{{deposit_types}}', 'id');
        $this->addForeignKey('fk_user_id_transactions','{{user_transactions}}', 'user_id', '{{users}}', 'id');
 	}
 
