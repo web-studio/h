9a58aa960aab2ec70092c71f89c062ed22339b7b
web-studio <vanechka_petrov_1996@list.ru>
2014-03-07 16:53:32 +0400

    баланс

--- public_html/themes/profit/views/admin/money/balance.php
+++ public_html/themes/profit/views/admin/money/balance.php
@@ -1,2 +1,8 @@
-<?php
-var_dump($balances);
+
+<table class="">
+    <tr>
+        <?php foreach ( $balances as $purse=>$value ) : ?>
+            <td><?= $purse ?></td><td><?= $value ?></td>
+        <?php endforeach ?>
+    </tr>
+</table>
