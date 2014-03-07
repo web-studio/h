0478d2347e95d90eb92cc78ee791256c7d47d4a0
web-studio <vanechka_petrov_1996@list.ru>
2014-03-07 17:54:20 +0400

    баланс

--- public_html/protected/modules/admin/controllers/MoneyController.php
+++ public_html/protected/modules/admin/controllers/MoneyController.php
@@ -17,7 +17,7 @@ class MoneyController extends AdminController
             ->order('expire ASC')
             ->where("status=1 AND expire>=NOW() AND expire<=DATE('" . date('Y-m-d h:i:s', time() + $this->expirationDate * 86400) . "')")
             ->queryAll();
-
+        var_dump($deps);die;
         /*$deposits = new CActiveDataProvider('UserDeposit',
             array(
                 'criteria' => array(
